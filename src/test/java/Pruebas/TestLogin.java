5fad34494f8827d84565776739368a14239d9cc9
Guido Robles <russellxp@gmail.com>
2021-01-11 20:38:48 -0500

    grid 3

--- src/test/java/Pruebas/TestLogin.java
+++ src/test/java/Pruebas/TestLogin.java
@@ -6,22 +6,27 @@ import org.testng.annotations.*;
 import paginas.PaginaHome;
 import paginas.PaginaLogin;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.lang.Object;
 
 //import java.io.File;
 //import java.io.IOException;
 
 //import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.FileDetector;
+import org.openqa.selenium.remote.LocalFileDetector;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class TestLogin {
 
-	WebDriver driver;
+	static RemoteWebDriver driver;
 //	String driverPath = "google-chrome";
 //	String driverPath = "./Drivers/chromedriver.exe";
 //	String driverPath = "/var/jenkins_home/workspace/Java_App_Maven/Drivers/chromedriver";
@@ -31,13 +36,39 @@ public class TestLogin {
 	@BeforeSuite
 	public void setUp() throws MalformedURLException {
 		
+		System.out.println("Chrome");
+	//	DesiredCapabilities cap = DesiredCapabilities.chrome();
+		//					cap.setPlatform(Platform.LINUX);
+			//				cap.setVersion("");
 		ChromeOptions chromeOptions = new ChromeOptions();
-		chromeOptions.setCapability("browserVersion", "87");
-		chromeOptions.setCapability("platformName", "LINUX");
-		//chromeOptions.setCapability("browserName", "chrome");
+	/*	chromeOptions.setCapability("browserVersion", "87");
+		chromeOptions.setCapability("platformName", Platform.LINUX);
+		chromeOptions.setCapability("browserName", "chrome");
+		*/
+		
+	       chromeOptions.setCapability("browserName", "chrome"); //To specify the browser
+	       chromeOptions.setCapability("version", "70.0"); //To specify the browser version
+	       chromeOptions.setCapability("platform", "win10"); // To specify the OS
+	       chromeOptions.setCapability("build", "LambdaTestApp"); //To identify the test
+	       chromeOptions.setCapability("name", "LambdaTestGridExample");
+	       chromeOptions.setCapability("network", true); // To enable network logs
+	       chromeOptions.setCapability("visual", true); // To enable step by step screenshot
+	       chromeOptions.setCapability("video", true); // To enable video recording
+	       chromeOptions.setCapability("console", true); // To capture console logs
+
+		
+	//	System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver");
+		//nodeUrl = "http://192.168.100.4:4444/wd/hub";
+		//DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+		//capabilities.setPlatform(Platform.SIERRA);
+		//ChromeOptions options = new ChromeOptions();
+		//options.merge(capabilities);
+		//driver = new RemoteWebDriver(new URL(nodeUrl), options);
+		//driver.get("http://www.amazon.com");
+		
 		
 		driver = new RemoteWebDriver(new URL("http://192.168.1.14:4446/wd/hub"), chromeOptions);
-		
+		driver.setFileDetector(new LocalFileDetector());
 		
 		driver.get("https://test.hospiceworks.com/login");
 		driver.quit();
